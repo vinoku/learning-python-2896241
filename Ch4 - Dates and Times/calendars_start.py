d85cd8bdeabdde571723a05b8d249e2c4969994d
vinoku <vinoku78@gmail.com>
2022-05-29 20:57:19 +0200

    Finish chapter 4

--- Ch4 - Dates and Times/calendars_start.py
+++ Ch4 - Dates and Times/calendars_start.py
@@ -5,21 +5,30 @@
 
 
 # TODO: import the calendar module
-
+import calendar
 
 # TODO: create a plain text calendar
-
+c = calendar.TextCalendar(calendar.MONDAY)
+str = c.formatmonth(2022, 1, 0, 0)
+print(str)
 
 # TODO: create an HTML formatted calendar
-
+hc = calendar.HTMLCalendar(calendar.MONDAY)
+str1 = hc.formatmonth(2022, 1)
+print(str1)
 
 # TODO: loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
-
+for i in c.itermonthdays(2022, 8):
+    print(i)
   
 # TODO: The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
+for name in calendar.month_name:
+    print(name)
 
+for day in calendar.day_name:
+    print(day)
 
 # TODO: Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
--- Ch4 - Dates and Times/timedeltas_start.py
+++ Ch4 - Dates and Times/timedeltas_start.py
@@ -7,29 +7,40 @@
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
+from time import time_ns
 
 
 # TODO: construct a basic timedelta and print it
-
+print(timedelta(days=365, hours=5, minutes=1))
 
 # TODO: print today's date
-
+now = datetime.now()
+print("Today's date is", now)
 
 # TODO: print today's date one year from now
-
+print("One year from now it will be", str(now + timedelta(days=365)))
 
 # TODO: create a timedelta that uses more than one argument
-
+print("In two weeks and 3 days it will be", str(now + timedelta(weeks=2, days=3)))
 
 # TODO: calculate the date 1 week ago, formatted as a string
-
+t = now - timedelta(weeks=1)
+s = t.strftime("%A %B %d, %Y")
+print("One week ago it was", s)
 
 ### How many days until April Fools' Day?
+today1 = date.today()
+afd = date(today1.year, 4, 1)
 
 
 # TODO: use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
-
+if afd < today1:
+    print("April Fools' Day ale=ready went by:", ((today1 - afd).days))
+    afd = afd.replace(year=today1.year + 1)
 
 # TODO: Now calculate the amount of time until April Fool's Day  
+time_to_afd = afd - today1
+print("It is", time_to_afd.days, " days until the next April Fools' Day!")
 
